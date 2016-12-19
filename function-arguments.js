25a9748dfbe327d0ceeedade3bb7d7fd83e09a5c
Nick Kasten <kastencode@gmail.com>
2016-12-18 23:22:56 -0600

    finished all the challenges. woop!

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+var math = function(term1, term2, term3) {
+	return term1 + (term2 * term3);
+}
+
+console.log(math(53,61,67));
---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit = 'orange';
+var output;
+if (fruit.length <= 5) {
+	output = 'The fruit name has five characters or less.';
+} else {
+	output = 'The fruit name has more than five characters.';
+}
+
+console.log(output);
